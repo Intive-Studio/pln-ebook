39c7ffff21cb46c04e5182e89e4c6aeded1d8726
Dzulfikri29 <35086896+Dzulfikri29@users.noreply.github.com>
2023-09-09 22:14:18 +0700

    [FINAL ANIMATION]

--- storage/framework/views/8d26bc1df1151383e9d0126ac448e47659dd5bf3.php
+++ storage/framework/views/8d26bc1df1151383e9d0126ac448e47659dd5bf3.php
@@ -20,13 +20,10 @@
     <link rel="shortcut icon" href="<?php echo e(asset('favico.ico')); ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo e(asset('e-catalog/css/wow_book.css')); ?> " type="text/css" />
     <link rel="stylesheet" type="text/css" href="<?php echo e(asset('e-catalog/css/main.css')); ?> ">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('e-catalog/css/icon.css')); ?> ">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('e-catalog/css/animate.css')); ?> ">
     <link rel="stylesheet" media="screen" type="text/css" href="<?php echo e(asset('e-catalog/css/style.css')); ?> ">
     <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/styles.css')); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?php echo e(asset('e-catalog/css/font-awesome.min.css')); ?> ">
-    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('e-catalog/css/toastr.min.css')); ?> ">
     <link rel="stylesheet" href="<?php echo e(asset('e-catalog/css/animation.css')); ?>?v=2">
 
     <style>
@@ -82,7 +79,6 @@
 <body style="overflow: hidden">
     <audio loop autoplay id="backsound">
         <source src="<?php echo e(asset('storage/video/backsound.mp3')); ?>">
-        
     </audio>
 
     <div class="container-fluid" style="height: 100vh; width: 100vw;">
@@ -91,14 +87,14 @@
                 <div id="katalog" class="buku">
                     <?php for($i = 1; $i <= 16; $i++): ?>
                         <?php
-                            $background = asset('e-catalog/images/content/' . $i . '.jpg?v=1');
+                            $background = asset('e-catalog/images/content/' . $i . '.jpg?v=2');
                         ?>
                         <div id="p<?php echo e($i + 1); ?>" style="background-image: url('<?php echo e($background); ?>');background-size: 100% 100%;" class="p-0">
                             <p style="visibility: hidden;font-size:0px;margin: 0px"><?php echo e($voice_overs[$i - 1] ?? ''); ?></p>
                             <?php echo $__env->make("svg.$i", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
                             <?php if($i == 4): ?>
                                 <div class="p-4 text-center">
-                                    <video width="95%" class="rounded" style="overflow:hidden;margin-top:22%" playsinline>
+                                    <video width="95%" class="rounded" style="overflow:hidden;margin-top:25%" playsinline preload="none">
                                         <source src="<?php echo e(asset('storage/video/video.mp4')); ?>" type="video/mp4">
                                         Your browser does not support the video tag.
                                     </video>
@@ -138,7 +134,6 @@
             </div>
         </div>
         <div class="position-relative">
-
             <div id="navigation2">
                 <a target="_blank" href="<?php echo e(asset('e-catalog/pln-nps-partnership-process.pdf')); ?>" class="btn btn-danger"><i class="fa fa-download"></i></a>
                 <button class="btn btn-lg btn-danger cov">
@@ -171,11 +166,7 @@
     <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/jquery-3.1.1.min.js')); ?>"></script>
     <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/jquery.hotkeys.js')); ?>"></script>
     <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/hotkeys.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/clipboard.min.js')); ?>"></script>
     <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/main.js')); ?>?v=<?php echo e(time()); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/semantic.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/toastr.min.js')); ?>"></script>
-    <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/stats.js')); ?>"></script>
     <script type="text/javascript" src="<?php echo e(asset('e-catalog/js/wow_book.min.js')); ?>"></script>
     <script>
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
